62501f699798cf8ce2b6e381148a2fd48bd0de4a
Robert Taraya <robert.taraya@gmail.com>
2015-07-01 01:20:53 -0500

    draw map to fit bounds of all points on map

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -7,26 +7,27 @@ $( document ).ready(function() {
 function initializeMap() {
 
   var mapCenter = new google.maps.LatLng(38.2590465, -122.5915786);
-
   var mapOptions = {
     zoom: 13,
     center: mapCenter
   }
-
   var mapCanvas = document.getElementById('map-canvas')
-
   var map = new google.maps.Map(mapCanvas, mapOptions);
+  var bounds = new google.maps.LatLngBounds();
 
   var wineryLocations = locations // locations was defined when winery#index was called
 
   for ( i = 0 ; i < wineryLocations.length ; i++ ) {
     var title = wineryLocations[i][0];
     var position = new google.maps.LatLng(wineryLocations[i][1], wineryLocations[i][2]);
+    bounds.extend(position);
 
     var marker = new google.maps.Marker({
         position: position,
         map: map,
         title: title
     });
+
+    map.fitBounds(bounds);
   }
 }
