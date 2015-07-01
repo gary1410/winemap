ec176ef6e268379fbe9512e5315bdde9253f2dbe
Robert Taraya <robert.taraya@gmail.com>
2015-07-01 01:20:17 -0500

    create method to return winery full address

--- app/models/winery.rb
+++ app/models/winery.rb
@@ -1,4 +1,8 @@
 class Winery < ActiveRecord::Base
-  geocoded_by :address
+  geocoded_by :full_address
   after_validation :geocode
+
+  def full_address
+    "#{address}, #{city}, #{zipcode}"
+  end
 end
