baaa451b14e2e7243b0c6a289bf9c5dfaac29d49
Robert Taraya <robert.taraya@gmail.com>
2015-06-30 19:37:09 -0500

    render multiple map markers for each hard-coded winery

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,25 +1,38 @@
 $( document ).ready(function() {
 
-  function initialize() {
-
-
-
-    var myLatlng = new google.maps.LatLng(38.2590465, -122.5915786);
-
-    var mapOptions = {
-      zoom: 13,
-      center: myLatlng
-    }
-
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title: 'Hello World!'
-    });
-  }
-
-  google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', initializeMap);
 
 });
+
+function initializeMap() {
+
+  var mapCenter = new google.maps.LatLng(38.2590465, -122.5915786);
+
+  var mapOptions = {
+    zoom: 13,
+    center: mapCenter
+  }
+
+  var mapCanvas = document.getElementById('map-canvas')
+
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+
+  var wineryLocations = [
+    ["CARNEROS VINTNERS", 38.2493358, -122.4981057],
+    ["ROBLEDO FAMILY WINERY", 38.2473916, -122.4834992],
+    ["COONEY PROPERTIES 20 LLC OWNER", 38.241682, -122.480983],
+    ["ANABA WINES", 44.9101929, -72.370795],
+    ["ELSBREE CHARLES N JR TR", 38.5554739, -122.783964]
+  ]
+
+  for ( i = 0 ; i < wineryLocations.length ; i++ ) {
+    var title = wineryLocations[i][0];
+    var position = new google.maps.LatLng(wineryLocations[i][1], wineryLocations[i][2]);
+
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: title
+    });
+  }
+}
