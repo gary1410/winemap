28e27617d83feba8c7ee020dc0229e061b730fee
theInternauts <cricketw@wearetheinternauts.com>
2015-08-07 01:11:08 -0700

    refactored to inject dependencies as much as possible

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,37 +1,36 @@
 HelloWinery = {}
-HelloWinery.Maps = {}
-HelloWinery.Maps.initializeMap = function() {
+HelloWinery.Maps = {
+  initializeMap: function(dataSet) {
+    var mapCenter = new google.maps.LatLng(38.2590465, -122.5915786);
+    var mapOptions = {
+      zoom: 20,
+      center: mapCenter
+    }
+    var mapCanvas = document.getElementById('map-canvas')
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var bounds = new google.maps.LatLngBounds();
 
-  var mapCenter = new google.maps.LatLng(38.2590465, -122.5915786);
-  var mapOptions = {
-    zoom: 20,
-    center: mapCenter
-  }
-  var mapCanvas = document.getElementById('map-canvas')
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  var bounds = new google.maps.LatLngBounds();
+    HelloWinery.Maps.mapLoop(map, bounds, dataSet);
+  },
 
-  HelloWinery.Maps.mapLoop(map, bounds);
+  mapLoop: function(map, bounds, dataSet) {
+    dataSet.forEach(function(winery) {
+      var config = {}
+      config['map'] = map
+      config['title'] = winery.title
+      config['position'] = new google.maps.LatLng(winery.latitude, winery.longitude)
+      bounds.extend(config['position'])
+      placeMarker(config, bounds)
+    })
 
-}
-
-HelloWinery.Maps.mapLoop = function (map, bounds) {
-  // gon.wineries is from the index.html.erb
-  gon.wineries.forEach(function(winery){
-    var config = {}
-    config['map'] = map
-    config['title'] = winery.title
-    config['position'] = new google.maps.LatLng(winery.latitude, winery.longitude)
-    bounds.extend(config['position'])
-    placeMarker(config)
-  })
-
-  function placeMarker(config){
-    var marker = new google.maps.Marker(config);
-    config['map'].fitBounds(bounds);
-  }
+    function placeMarker(config, bounds) {
+      var marker = new google.maps.Marker(config);
+      config['map'].fitBounds(bounds);
+    }
+  }  
 }
 
 $(document).ready(function() {
-  google.maps.event.addDomListener(window, 'load', HelloWinery.Maps.initializeMap);
+  // gon.wineries is from the index.html.erb
+  google.maps.event.addDomListener(window, 'load', HelloWinery.Maps.initializeMap.call(this, gon.wineries));
 });
