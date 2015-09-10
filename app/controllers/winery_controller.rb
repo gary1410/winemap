7d9b04b4d2afe2d0e5608b77585ea88225dc3b0e
Robert Taraya <robert.taraya@gmail.com>
2015-09-09 23:53:07 -0500

    create sample winery tour using first 5 wineries in db

--- app/controllers/winery_controller.rb
+++ app/controllers/winery_controller.rb
@@ -2,4 +2,17 @@ class WineryController < ApplicationController
   def index
     gon.wineries = Winery.all
   end
+
+  def directions
+    gon.winery_tour = extract_lat_lon_values
+  end
+
+  private
+
+  def extract_lat_lon_values
+    wineries = Winery.first(5)
+    lat_lon = wineries.map do |winery|
+      [winery.latitude, winery.longitude]
+    end
+  end
 end
